3983b37ff24c45823910dfd59374125fd404dfd0
Farhood <farhoodetaati@gmail.com>
2022-03-17 23:37:27 +0330

    adds tests for generator model

---
+++ tests/ml/model/test_generator.py
@@ -0,0 +1,31 @@
+from unittest import mock
+
+import pytest
+import torch
+
+from comic_cover_generator.ml.model import Discriminator, Generator
+
+
+class MockedPGAN:
+    def __init__(self):
+        pass
+
+    def getNetG(self):
+        def features(x: torch.Tensor, feature_size=(256, 256)):
+            if x.size()[-1] != Generator.latent_dim:
+                raise RuntimeError("Mismatch of input shape.")
+            return torch.rand(len(x), 3, *feature_size)
+
+        return features
+
+
+@pytest.fixture(scope="module")
+def gen():
+    with mock.patch("torch.hub.load") as mocked:
+        mocked.return_value = MockedPGAN()
+        yield Generator(pretrained=False)
+
+
+@torch.no_grad()
+def test_generator_resizer_shape_match_discriminator_input_shape(gen):
+    assert gen(torch.rand(1, gen.latent_dim)).size()[-2:] == Discriminator.input_shape
